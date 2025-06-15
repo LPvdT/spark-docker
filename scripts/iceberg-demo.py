ace6afab3dfe8d0da04caf3f08df806b74d3c2ca
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-06-15 12:55:40 +0200

    update iceberg-demo.py

--- scripts/iceberg-demo.py
+++ scripts/iceberg-demo.py
@@ -1,33 +1,45 @@
-from pyspark.sql import SparkSession
 from pyspark import SparkConf
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType, StringType, StructField, StructType
 
 warehouse_path = "./warehouse"
-iceberg_spark_jar = 'org.apache.iceberg:iceberg-spark-runtime-3.4_2.12:1.4.3'
-iceberg_spark_ext = 'org.apache.iceberg:iceberg-spark-extensions-3.4_2.12:1.4.3'
+iceberg_spark_jar = "org.apache.iceberg:iceberg-spark-runtime-3.4_2.12:1.9.1"
+iceberg_spark_ext = "org.apache.iceberg:iceberg-spark-extensions-3.4_2.12:1.9.1"
 catalog_name = "demo"
 
 # Setup iceberg config
-conf = SparkConf().setAppName("icebergDemo") \
-    .set("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
-    .set(f"spark.sql.catalog.{catalog_name}", "org.apache.iceberg.spark.SparkCatalog") \
-    .set('spark.jars.packages', iceberg_spark_jar) \
-    .set('spark.jars.packages', iceberg_spark_ext) \
-    .set(f"spark.sql.catalog.{catalog_name}.warehouse", warehouse_path) \
-    .set(f"spark.sql.catalog.{catalog_name}.type", "hadoop")\
+conf = (
+    SparkConf()
+    .setAppName("icebergDemo")
+    .set(
+        "spark.sql.extensions",
+        "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
+    )
+    .set(f"spark.sql.catalog.{catalog_name}", "org.apache.iceberg.spark.SparkCatalog")
+    .set("spark.jars.packages", iceberg_spark_jar)
+    .set("spark.jars.packages", iceberg_spark_ext)
+    .set(f"spark.sql.catalog.{catalog_name}.warehouse", warehouse_path)
+    .set(f"spark.sql.catalog.{catalog_name}.type", "hadoop")
     .set("spark.sql.defaultCatalog", catalog_name)
+)
 
 # Create spark session
 spark = SparkSession.builder.config(conf=conf).getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 
 # Create a dataframe
-schema = StructType([
-    StructField('name', StringType(), True),
-    StructField('age', IntegerType(), True),
-    StructField('job_title', StringType(), True)
-])
-data = [("person1", 28, "Doctor"), ("person2", 35, "Singer"), ("person3", 42, "Teacher")]
+schema = StructType(
+    [
+        StructField("name", StringType(), True),
+        StructField("age", IntegerType(), True),
+        StructField("job_title", StringType(), True),
+    ]
+)
+data = [
+    ("person1", 28, "Doctor"),
+    ("person2", 35, "Singer"),
+    ("person3", 42, "Teacher"),
+]
 df = spark.createDataFrame(data, schema=schema)
 
 # Create database
